bb837a8f7e3b1304a73d1b7ca52992972c29ebf4
Logan Rudd <loganrudd@gmail.com>
2025-03-25 16:58:42 -0300

    Update color scheme on sign in page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -12,7 +12,7 @@ export default function SignIn() {
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="pb-12 text-center">
-            <h1 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-green-200),var(--color-sky-200),var(--color-green-50),var(--color-sky-300),var(--color-green-200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
+            <h1 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-green-800),var(--color-green-800),var(--color-green-950),var(--color-green-950),var(--color-green-800))] bg-[length:800%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
               Welcome back
             </h1>
           </div>
@@ -21,7 +21,7 @@ export default function SignIn() {
             <div className="space-y-5">
               <div>
                 <label
-                  className="mb-1 block text-sm font-medium text-sky-200/65"
+                  className="mb-1 block text-sm font-medium text-green-800/65"
                   htmlFor="email"
                 >
                   Email
@@ -29,14 +29,14 @@ export default function SignIn() {
                 <input
                   id="email"
                   type="email"
-                  className="form-input w-full"
+                  className="form-input w-full placeholder-green-800/90"
                   placeholder="Your email"
                 />
               </div>
               <div>
                 <div className="mb-1 flex items-center justify-between gap-3">
                   <label
-                    className="block text-sm font-medium text-sky-200/65"
+                    className="block text-sm font-medium text-green-800/65"
                     htmlFor="password"
                   >
                     Password
@@ -51,13 +51,13 @@ export default function SignIn() {
                 <input
                   id="password"
                   type="password"
-                  className="form-input w-full"
+                  className="form-input w-full placeholder-green-800/90"
                   placeholder="Your password"
                 />
               </div>
             </div>
             <div className="mt-6 space-y-5">
-              <button className="btn w-full bg-linear-to-t from-sky-600 to-sky-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]">
+              <button className="btn w-full bg-linear-to-t from-green-600 to-green-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]">
                 Sign in
               </button>
               <div className="flex items-center gap-3 text-center text-sm italic text-green-600 before:h-px before:flex-1 before:bg-linear-to-r before:from-transparent before:via-green-400/25 after:h-px after:flex-1 after:bg-linear-to-r after:from-transparent after:via-green-400/25">
@@ -69,9 +69,9 @@ export default function SignIn() {
             </div>
           </form>
           {/* Bottom link */}
-          <div className="mt-6 text-center text-sm text-sky-200/65">
+          <div className="mt-6 text-center text-sm text-green-800/65">
             Don't you have an account?{" "}
-            <Link className="font-medium text-sky-500" href="/signup">
+            <Link className="font-medium text-green-500" href="/signup">
               Sign Up
             </Link>
           </div>
