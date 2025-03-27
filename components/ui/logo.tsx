488ee7296a121fa26e2b03d26ea874f7ebb94034
Logan Rudd <loganrudd@gmail.com>
2025-03-27 17:41:39 -0300

    Update company name and logo, and gitignore .next

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,8 +4,8 @@ import logo from "@/public/images/logo.png";
 
 export default function Logo() {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={32} height={32} />
+    <Link href="/" className="inline-flex shrink-0" aria-label="GroMoPo">
+      <Image src={logo} alt="GroMoPo Logo" width={32} height={32} />
     </Link>
   );
 }
