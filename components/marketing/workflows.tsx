f0b7ce62b8dae59dce3d7438cdd05328560fde2f
Logan Rudd <loganrudd@gmail.com>
2025-03-30 13:16:22 -0300

    Create dashboard and redirect when logging in/out of auth0, and finish default landing page

--- components/marketing/workflows.tsx
+++ components/marketing/workflows.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import WorflowImg01 from "@/public/images/workflow-01.png";
 import WorflowImg02 from "@/public/images/workflow-02.png";
 import WorflowImg03 from "@/public/images/workflow-03.png";
-import Spotlight from "@/components/spotlight";
+import Spotlight from "@/components/marketing/spotlight";
 
 export default function Workflows() {
   return (
