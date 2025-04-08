dce2162c70b04648383956dd2d1d3852c4d172ba
Logan Rudd <loganrudd@gmail.com>
2025-04-08 19:16:43 -0300

    Revert "Implement custom email action handlers"
    This reverts commit d90d13f8f12f098e1885c50a5dc0f40ed9f87eed.

--- app/auth/email-verrified/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function EmailVerified() {
-    return (
-      <div className="max-w-md mx-auto p-6 bg-white rounded shadow mt-10">
-        <h1 className="text-2xl font-bold mb-4">Email Verified</h1>
-        <p>Your email has been successfully verified. We'll email you as soon as we have updates.</p>
-      </div>
-    );
-  }
