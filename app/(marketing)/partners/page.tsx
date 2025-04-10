ae6e91b51f9721998230f71a382b56d19154cca5
Logan Rudd <loganrudd@gmail.com>
2025-04-10 14:04:07 -0300

    Update content of partners page, and add footer to both partners and signup pages

--- app/(marketing)/partners/page.tsx
+++ app/(marketing)/partners/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import PartnersForm from "@/components/marketing/partners-form";
+import Footer from "@/components/marketing/ui/footer";
 
 export default function SignupPage() {
   return (
-    
-    <main className="flex items-center justify-center p-12 mx-auto max-w-6xl px-4 sm:px-6">
-      <PartnersForm />
-    </main>
+    <>
+      <main className="flex items-center justify-center p-12 mx-auto max-w-6xl px-4 sm:px-6">
+        <PartnersForm />
+      </main>
+      <Footer />
+    </>
   );
 }
