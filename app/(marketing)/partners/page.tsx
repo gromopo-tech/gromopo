4a1a784da9199ccc815eecba694300f503ec207b
Logan Rudd <loganrudd@gmail.com>
2025-04-10 12:44:01 -0300

    Move signup page to marketing, and add form for partners

---
+++ app/(marketing)/partners/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import PartnersForm from "@/components/partners-form";
+
+export default function SignupPage() {
+  return (
+    
+    <main className="flex items-center justify-center p-12 mx-auto max-w-6xl px-4 sm:px-6">
+      <PartnersForm />
+    </main>
+  );
+}
