3c1d03fc6dfdae4353fae8e02cef8ef4465c80f2
Logan Rudd <loganrudd@gmail.com>
2025-04-21 15:23:55 -0300

    Imporve add/delete task buttons in schedule

--- app/(account)/dashboard/scheduling/page.tsx
+++ app/(account)/dashboard/scheduling/page.tsx
@@ -507,7 +507,9 @@ export default function SchedulingPage() {
                                       onClick={() => handleRemoveTask(employee.id, dateStr, index)}
                                       className="text-red-500 hover:text-red-700"
                                     >
-                                      <Minus className="w-3 h-3" />
+                                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                                      </svg>
                                     </button>
                                   </div>
                                 );
@@ -515,7 +517,7 @@ export default function SchedulingPage() {
                               {availableTasks.length > 0 && (
                                 <button
                                   onClick={() => setSelectedCell({ employeeId: employee.id, date: dateStr })}
-                                  className="flex items-center justify-center w-full p-1 hover:text-gray-700 border border-dashed border-gray-300 rounded"
+                                  className="flex items-center justify-center w-full p-1 text-gray-500 hover:bg-gray-100 border border-dashed border-gray-300 rounded transition-colors duration-200"
                                 >
                                   <Plus className="w-3 h-3" />
                                 </button>
