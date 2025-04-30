6722165e6f8c3c3017e4894a358a27941986e363
Logan Rudd <loganrudd@gmail.com>
2025-04-30 14:35:31 -0300

    Add Terms of Service and Privacy Policy

--- components/marketing/ui/footer.tsx
+++ components/marketing/ui/footer.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/marketing/ui/logo";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -11,12 +12,27 @@ export default function Footer() {
         >
         </div>
         <div className="grid grid-cols-2 justify-between gap-12 py-8 sm:grid-rows-[auto_auto] md:grid-cols-4 md:grid-rows-[auto_auto] md:py-12 lg:grid-cols-[repeat(4,minmax(0,140px))_1fr] lg:grid-rows-1 xl:gap-20">
-          {/* 5th block */}
+          {/* 1st block */}
           <div className="flex items-center space-x-2">
             <Logo />
-            <span className="text-lg font-semibold text-green-800/65">
+            <span className="text-lg font-semibold text-green-800/65 transition hover:text-green-500">
             GroMoPo
             </span>
+          </div>
+          {/* 2nd block */}
+          <div className="flex items-center space-x-2 justify-right">
+            <a
+              className="text-green-800/65 transition hover:text-green-500"
+              href="/terms-of-service"
+            >
+              Terms of service
+            </a>
+            <a
+              className="text-green-800/65 transition hover:text-green-500"
+              href="/terms-of-service"
+            >
+              Privacy policy
+            </a>
           </div>
         </div>
       </div>
