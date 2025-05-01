d69d2d898a8bbfc124a40322577448269ad7669f
Logan Rudd <loganrudd@gmail.com>
2025-05-01 09:15:21 -0300

    Change gCal api scope to read only and use primary calendar of connected account (#32)

--- components/marketing/ui/footer.tsx
+++ components/marketing/ui/footer.tsx
@@ -7,20 +7,19 @@ export default function Footer() {
       <div className="relative mx-auto max-w-6xl px-4 sm:px-6">
         {/* Footer illustration */}
         <div
-          className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -trangreen-x-1/2"
+          className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -translate-x-1/2"
           aria-hidden="true"
-        >
-        </div>
+        ></div>
         <div className="grid grid-cols-2 justify-between gap-12 py-8 sm:grid-rows-[auto_auto] md:grid-cols-4 md:grid-rows-[auto_auto] md:py-12 lg:grid-cols-[repeat(4,minmax(0,140px))_1fr] lg:grid-rows-1 xl:gap-20">
           {/* 1st block */}
           <div className="flex items-center space-x-2">
             <Logo />
             <span className="text-lg font-semibold text-green-800/65 transition hover:text-green-500">
-            GroMoPo
+              GroMoPo
             </span>
           </div>
           {/* 2nd block */}
-          <div className="flex items-center space-x-2 justify-right">
+          <div className="flex items-center space-x-4 justify-end">
             <a
               className="text-green-800/65 transition hover:text-green-500"
               href="/terms-of-service"
@@ -29,7 +28,7 @@ export default function Footer() {
             </a>
             <a
               className="text-green-800/65 transition hover:text-green-500"
-              href="/terms-of-service"
+              href="/privacy-policy"
             >
               Privacy policy
             </a>
