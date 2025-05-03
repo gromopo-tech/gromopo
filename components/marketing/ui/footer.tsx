48faf2ea796621911fddc9ea1b6cadbfb8cc7ed1
Logan Rudd <loganrudd@gmail.com>
2025-05-03 09:04:45 -0300

    Make signin page the home page, improve footer formatting, and redirect authenticated users from home page to dashboard

--- components/marketing/ui/footer.tsx
+++ components/marketing/ui/footer.tsx
@@ -1,5 +1,4 @@
 import Logo from "@/components/marketing/ui/logo";
-import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -10,7 +9,7 @@ export default function Footer() {
           className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -translate-x-1/2"
           aria-hidden="true"
         ></div>
-        <div className="grid grid-cols-2 justify-between gap-12 py-8 sm:grid-rows-[auto_auto] md:grid-cols-4 md:grid-rows-[auto_auto] md:py-12 lg:grid-cols-[repeat(4,minmax(0,140px))_1fr] lg:grid-rows-1 xl:gap-20">
+        <div className="flex items-center justify-between py-8 md:py-12">
           {/* 1st block */}
           <div className="flex items-center space-x-2">
             <Logo />
@@ -19,7 +18,7 @@ export default function Footer() {
             </span>
           </div>
           {/* 2nd block */}
-          <div className="flex items-center space-x-4 justify-end">
+          <div className="flex items-center space-x-4">
             <a
               className="text-green-800/65 transition hover:text-green-500"
               href="/terms-of-service"
