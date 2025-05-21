9200dd01352d89c48c777102d621cf2368e37609
Logan Rudd <loganrudd@gmail.com>
2025-05-21 10:37:25 -0300

    Auth roles (#36)
    * Add basic functionanlity for admins to add/edit employees and their roles
    
    * Anyone can delete employees, only admin/owner can create taker/maker, only owner can create admin, but admin can turn a taker/maker into admin, but can't turn an admin to taker/maker, taker/maker can't edit or create
    
    * Maker/Taker roles cannot create/edit/delete any employees
    
    * Refactor lib functions for getting user data
    
    * Remove maker access to dashboard
    
    * Remove maker access to /take
    
    * Ristrict option available in header dropdown depending on role
    
    * Remove taker access to dashboard/employees
    
    * Show owner/admin employee actions as disabled to other admins, and remove the admin option from create-employee page for admins
    
    * Only allow owners to change a taker/maker role to admin
    
    * Correct layout names
    
    * Use server components for all all layouts but root, redirect all traffic from homepage to signin page, and configure redirects depending on auth state/role
    
    * Revert redirects and remove workflows from landing page

--- app/(protected)/dashboard/menus/page.tsx
+++ app/(protected)/dashboard/menus/page.tsx
@@ -1,10 +1,10 @@
-export default function OrderPage() {
+export default async function DashboardPage() {
   return (
       <div className="flex flex-col w-full h-full p-6 space-y-6">
       <div className="grid gap-4 md:grid-cols-3">
           <div className="p-4 bg-white rounded shadow-md">
-          <h2 className="text-lg font-semibold text-gray-600">Order</h2>
-          <p className="text-gray-600">Place your order here.</p>
+          <h2 className="text-lg font-semibold text-gray-600">Menus</h2>
+          <p className="text-gray-600">Add/edit menus here.</p>
           </div>
       </div>
       </div>
