ed75b2e4ed3789e0ed00d41b86c609b465ff5d1a
Logan Rudd <loganrudd@gmail.com>
2025-05-22 09:33:20 -0300

    Show only privacy policy in footer

--- components/marketing/ui/footer.tsx
+++ components/marketing/ui/footer.tsx
@@ -17,14 +17,8 @@ export default function Footer() {
               GroMoPo
             </span>
           </div>
-          {/* 2nd block 
+          {/* 2nd block */}
           <div className="flex items-center space-x-4">
-            <a
-              className="text-green-800/65 transition hover:text-green-500"
-              href="/terms-of-service"
-            >
-              Terms of service
-            </a>
             <a
               className="text-green-800/65 transition hover:text-green-500"
               href="/privacy-policy"
@@ -32,7 +26,6 @@ export default function Footer() {
               Privacy policy
             </a>
           </div>
-          */}
         </div>
       </div>
     </footer>
