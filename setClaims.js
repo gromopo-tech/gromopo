2878b1886215e77be65bc7e214fc9299ef95a892
Logan Rudd <loganrudd@gmail.com>
2025-06-05 12:31:42 -0300

    Set display name in setClaims script too

--- setClaims.js
+++ setClaims.js
@@ -13,9 +13,17 @@ async function setClaims() {
   const uid = ''; // Replace with the user's UID
   const businessId = ''; // Replace with the businessId
   const role = 'owner'; // first user of a business should be an owner
+  const displayName = ''; // Optionally set the display name
 
+  // Set custom claims
   await adminAuth.setCustomUserClaims(uid, { role, businessId });
   console.log(`Custom claims set for user ${uid}:`, { businessId, role });
+
+  // Set displayName if provided
+  if (displayName) {
+    await adminAuth.updateUser(uid, { displayName });
+    console.log(`Display name set for user ${uid}:`, displayName);
+  }
 }
 
 setClaims().catch(console.error);
