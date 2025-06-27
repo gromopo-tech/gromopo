bc5bb02462c7ea643a4226ff7297a648f24ea6e9
Logan Rudd <loganrudd@gmail.com>
2025-06-27 15:52:10 -0300

    Update marketing

--- src/components/customer/cta.tsx
+++ src/components/customer/cta.tsx
@@ -7,18 +7,18 @@ export default function Cta() {
       >
       </div>
       <div className="max-w6xl mx-auto px-4 sm:px-6">
-        <div className="bg-linear-to-r from-transparent via-gray-800/50 py-12 md:py-20">
+        <div className="bg-linear-to-r from-transparent via-fuchsia-500 py-12 md:py-20">
           <div className="mx-auto max-w-3xl text-center">
             <h2
-              className="bg-gradient-to-r from-gray-400 via-gray-600 to-gray-400 bg-clip-text text-transparent animate-gradient-x pb-8 text-3xl font-semibold text-transparent md:text-4xl"
+              className="bg-gradient-to-r from-fuchsia-500 to-cyan-400 bg-clip-text text-transparent animate-gradient-x pb-8 text-3xl font-semibold text-transparent md:text-4xl"
               data-aos="fade-up"
             >
               Join the customer-first platform
             </h2>
             <div className="mx-auto flex flex-col sm:flex-row gap-6 sm:justify-center">
-              <div className="flex-1 sm:flex-none" data-aos="fade-up" data-aos-delay={400}>
+              <div className="flex flex-col sm:flex-row gap-4" data-aos="fade-up" data-aos-delay={400}>
                 <a
-                  className="btn w-full border hover:bg-neutral-100 dark:hover:bg-neutral-800 bg-neutral-200 dark:bg-neutral-700 text-gray-900 dark:text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-500"
+                  className="btn bg-gradient-to-r from-fuchsia-500 to-cyan-400 text-white dark:text-black hover:from-fuchsia-600 hover:to-cyan-500 px-2 py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-fuchsia-400"
                   href="signup"
                 >
                   <span className="relative inline-flex items-center">
@@ -28,10 +28,8 @@ export default function Cta() {
                     </span>
                   </span>
                 </a>
-              </div>
-              <div className="flex-1 sm:flex-none" data-aos="fade-up" data-aos-delay={600}>
                 <a
-                  className="btn w-full border hover:bg-neutral-100 dark:hover:bg-neutral-800 bg-neutral-200 dark:bg-neutral-700 text-gray-900 dark:text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-500"
+                  className="btn bg-cyan-100 text-cyan-800 hover:bg-cyan-200 text-black px-2 py-2 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400"
                   href="partners"
                 >
                   <span className="relative inline-flex items-center">
