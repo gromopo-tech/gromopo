78220e29202473787974b15c98f188153346865a
Logan Rudd <loganrudd@gmail.com>
2025-08-06 11:37:04 -0300

    Fix build errors

--- bigint-buffer-safe/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'bigint-buffer' {
-  export function toBigIntLE(buf: Buffer, offset?: number): bigint;
-  export function toBigIntBE(buf: Buffer, offset?: number): bigint;
-  export function toBufferLE(value: bigint | number, size: number): Buffer;
-  export function toBufferBE(value: bigint | number, size: number): Buffer;
-}
