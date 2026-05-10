02b72e23c5646d883742f2b52612400e6e4b4386
Logan Rudd <loganrudd@gmail.com>
2026-05-09 23:11:56 -0300

    Fix lint errors

--- src/app/(subdomains)/layout.tsx
+++ src/app/(subdomains)/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { HomeRedirectIfAuthenticated } from "@/components/marketing/home-redirect-if-auth";
 import { AppHeaderSubdomain } from '@/components/app-header-subdomain';
 
 export default function SubdomainLayout({ children }: { children: React.ReactNode }) {
